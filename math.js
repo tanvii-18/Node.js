93e683d6c07ce7100b0686c6bbcf1094c19e3dce
tanvii-18 <8438tanvipatel@gmail.com>
2025-11-06 18:41:31 +0530

    first commit

---
+++ math.js
@@ -0,0 +1,19 @@
+export const areaOfTriangle = (b, h) => {
+  let tringle = (b * h) / 2;
+  console.log(tringle);
+};
+
+export const areaOfCircle = (r) => {
+  let circle = Math.PI * r * r;
+  console.log(circle);
+};
+
+export const areaOfSquare = (a) => {
+  let side = a * a;
+  console.log(side);
+};
+
+export const areaOfRectengle = (len, wid) => {
+  let area = len * wid;
+  console.log(area);
+};
