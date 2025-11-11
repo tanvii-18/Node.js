2defc2ad204c5e48eb2b7c7c8e04857a5001567f
tanvii-18 <8438tanvipatel@gmail.com>
2025-11-11 09:13:33 +0530

    11-11-25

---
+++ core module/file-base web-server/web-server.js
@@ -0,0 +1,71 @@
+// Tasks
+// Create an HTTP server using the http module on port 5000.
+
+// Serve HTML pages using fs.readFile():
+
+// / → index.html
+// /about → about.html
+// /contact → contact.html
+// Any other → 404.html
+// Use path module to handle file paths.
+
+// Log each request (URL + time) in logs/server.log.
+
+// (Bonus) Add /data route that returns JSON response.
+
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  let filepath;
+
+  if (req.url === "/") {
+    filepath = path.join(__dirname, "home.html");
+    fs.readFile(filepath, (err, data) => {
+      if (err) {
+        res.writeHead(500, { "content-type": "text/plain" });
+        res.end("server Error");
+      } else {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end(data);
+      }
+    });
+  }
+  //   about page
+  else if (req.url == "/about") {
+    filepath = path.join(__dirname, "about.html");
+    fs.readFile(filepath, (err, data) => {
+      if (err) {
+        res.writeHead(500, { "content-type": "text/plain" });
+        res.end("server Error");
+      } else {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end(data);
+      }
+    });
+  }
+
+  //   contact page
+  else if (req.url == "/contact") {
+    filepath = path.join(__dirname, "contact.html");
+    fs.readFile(filepath, (err, data) => {
+      if (err) {
+        res.writeHead(500, { "content-type": "text/plain" });
+        res.end("server Error");
+      } else {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end(data);
+      }
+    });
+  }
+  //   client error
+  else {
+    res.writeHead(404, { "content-type": "text/plain" });
+    res.end("404 page not found!");
+  }
+});
+
+server.listen(5000, () => {
+  console.log("http://localhost:5000");
+});
