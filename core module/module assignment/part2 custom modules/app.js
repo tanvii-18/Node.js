473ae6b137c880de6df5a7c0e1ed4f54178781ad
tanvii-18 <8438tanvipatel@gmail.com>
2025-11-11 08:30:03 +0530

    11-11-25

--- core module/module assignment/part2 custom modules/app.js
+++ core module/module assignment/part2 custom modules/app.js
@@ -2,6 +2,13 @@
 
 const name = require("./name.js");
 const sum = require("./sum.js");
+const { add, sub, multiply, divide } = require("./mathematical-operations.js");
 
 name("tanvi");
 sum(4, 5);
+
+// mathematical
+add(7, 8);
+sub(9, 6);
+multiply(6, 9);
+divide(20, 8);
--- core module/module assignment/part2 custom modules/mathematical-operations.js
+++ core module/module assignment/part2 custom modules/mathematical-operations.js
@@ -1,1 +1,19 @@
 // Create a module with multiple functions: add, subtract, multiply, and divide — import all and use them.
+
+const add = (a, b) => {
+  console.log(a + b);
+};
+
+const sub = (a, b) => {
+  console.log(a - b);
+};
+
+const multiply = (a, b) => {
+  console.log(a * b);
+};
+
+const divide = (a, b) => {
+  console.log(a / b);
+};
+
+module.exports = { add, sub, multiply, divide };
