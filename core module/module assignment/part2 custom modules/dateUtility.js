0e3d2a07d5b7a5625b5e8ae7565a4daa3312d7c9
tanvii-18 <8438tanvipatel@gmail.com>
2025-11-12 09:35:03 +0530

    12-11-25

--- core module/module assignment/part2 custom modules/dateUtility.js
+++ core module/module assignment/part2 custom modules/dateUtility.js
@@ -1,1 +1,13 @@
 // Create a date utility module that exports today’s date and current time.
+
+const dateTimeUtility = () => {
+  const now = new Date();
+
+  const date = now.getDate();
+  const month = now.getMonth() + 1;
+  const year = now.getFullYear();
+
+  console.log(date, month, year);
+};
+
+dateTimeUtility();
--- core module/module assignment/part2 custom modules/randomNum.js
+++ core module/module assignment/part2 custom modules/randomNum.js
@@ -1,1 +1,9 @@
 // Create a custom module to generate a random number between a given range.
+
+const randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const getRandomNum = randomNumber(0, 10);
+
+console.log(getRandomNum);
