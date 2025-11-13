8a81bc1dc7c780d32715ec3ad3af63043bdc2371
tanvii-18 <8438tanvipatel@gmail.com>
2025-11-13 10:57:12 +0530

    13-11-25

--- express Js/file-getRequest.js
+++ express Js/file-getRequest.js
@@ -1,1 +1,19 @@
 // Create a server that writes the current date and time in a file every time a GET request is received.
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const now = new Date();
+
+  //   const hour = now.getHours();
+  //   const min = now.getMinutes();
+  //   const sec = now.getSeconds();
+  //   console.log(hour, min, sec);
+  res.send(`${now}`);
+});
+
+app.listen(3000, () => {
+  console.log("http://localhost:3000");
+});
