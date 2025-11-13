05cff5602e27562447ffe20d6abc7c6eb4a2d7df
tanvii-18 <8438tanvipatel@gmail.com>
2025-11-13 10:15:12 +0530

    13-11-25

---
+++ express Js/app.js
@@ -0,0 +1,3 @@
+// Create a basic express Js server that returns “Server is Running Successfully”.
+// Create a server that returns JSON data when accessed from the browser.
+// Create a server that writes the current date and time in a file every time a GET request is received.
