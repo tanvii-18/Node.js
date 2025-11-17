240dc9eada4ac4105d5fadf18c7e96c442fa3d67
tanvii-18 <8438tanvipatel@gmail.com>
2025-11-17 22:10:45 +0530

    17-11-25

--- Todo-Node.js/src/App.jsx
+++ Todo-Node.js/src/App.jsx
@@ -1,17 +1,24 @@
+import { useState } from "react";
 import "./index.css";
 
 function App() {
+  const [todo, setTodo] = useState([]);
   return (
-    <div>
-      <h2>Todo App</h2>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h2 className="m-5">Todo App</h2>
 
-      <div className="min-h-screen flex flex-col items-center justify-center gap-4">
+      <div className="flex flex-row items-center justify-center gap-4">
         <input
           type="text"
           placeholder="Add todo..."
-          className="h-[40px] w-[25%] bg-amber-600 rounded-2xl p-2"
+          className="h-[40px] w-[100%] bg-amber-600 rounded-2xl p-2"
         />
         <button>Add Todo</button>
+      </div>
+
+      {/* showing todos */}
+      <div>
+        <p>Todo</p>
       </div>
     </div>
   );
