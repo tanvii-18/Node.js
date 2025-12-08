de58f4c8ce1dba7fe21514ca4a927dab33b1820b
tanvii-18 <8438tanvipatel@gmail.com>
2025-12-08 17:11:23 +0530

    update deleteMovieController.js and movieRoute.js

--- mongoDB/movie_project/backend/controllers/deleteMovieController.js
+++ mongoDB/movie_project/backend/controllers/deleteMovieController.js
@@ -1,1 +1,22 @@
 import movies from "../models/movieModel.js";
+import { ObjectId } from "mongodb";
+
+export const deleteMovies = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "Invalid Movie ID format" });
+    }
+
+    const result = await movies.deleteOne({ _id: new ObjectId(id) });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ message: "Movie not found" });
+    }
+
+    res.json({ message: "Product deleted successfully!" });
+  } catch (error) {
+    res.status(500).json({ message: "delete Failed!", error: message });
+  }
+};
--- mongoDB/movie_project/backend/routes/movieRoute.js
+++ mongoDB/movie_project/backend/routes/movieRoute.js
@@ -2,6 +2,7 @@ import expres from "express";
 import { getMovieById, getMovies } from "../controllers/getMovieController.js";
 import { addMovie } from "../controllers/addMovieController.js";
 import { updateMovie } from "../controllers/updateMovieController.js";
+import { deleteMovies } from "../controllers/deleteMovieController.js";
 
 const router = expres.Router();
 
@@ -9,5 +10,6 @@ router.get("/", getMovies);
 router.get("/:id", getMovieById);
 router.post("/post", addMovie);
 router.put("/update/:id", updateMovie);
+router.delete("/delete/:id", deleteMovies);
 
 export default router;
