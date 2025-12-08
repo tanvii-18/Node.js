835ec67d43b9be2a40df0e37b42c3fb4b2618c82
tanvii-18 <8438tanvipatel@gmail.com>
2025-12-08 06:52:54 +0530

    8-12-25

--- mongoDB/movie_project/backend/controllers/updateMovieController.js
+++ mongoDB/movie_project/backend/controllers/updateMovieController.js
@@ -1,1 +1,26 @@
+import mongoose from "mongoose";
 import movies from "../models/movieModel.js";
+
+export const updateMovie = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "Invalid Id!" });
+    }
+
+    const updateData = await movies.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+
+    if (!updateData) {
+      return res.status(404).json({ message: "movie not found!" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "movie-infos updated! ", movies: updateData });
+  } catch (error) {
+    res.status(400).json({ message: "update failed", error: error.message });
+  }
+};
--- mongoDB/movie_project/backend/routes/movieRoute.js
+++ mongoDB/movie_project/backend/routes/movieRoute.js
@@ -1,11 +1,13 @@
 import expres from "express";
 import { getMovieById, getMovies } from "../controllers/getMovieController.js";
 import { addMovie } from "../controllers/addMovieController.js";
+import { updateMovie } from "../controllers/updateMovieController.js";
 
 const router = expres.Router();
 
 router.get("/", getMovies);
 router.get("/:id", getMovieById);
 router.post("/post", addMovie);
+router.put("/update/:id", updateMovie);
 
 export default router;
