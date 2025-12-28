80aff3f14e0660324916ecccc97e380abb70ead7
tanvii-18 <8438tanvipatel@gmail.com>
2025-12-28 21:27:25 +0530

    28-12-25

--- mongoDB/simple auth/simple_auth.js
+++ mongoDB/simple auth/simple_auth.js
@@ -5,6 +5,7 @@ const password = "mySecurePassword";
 const hashedPassword = await bcrypt.hash(password, 10);
 // .hash makes the combination of mix letters and numbers
 // 10 is salt rounds makes the password more secure after .hash method
+// salt : Salt is random data added to a password before hashing.
 
 console.log("Hashed Password:", hashedPassword);
 
