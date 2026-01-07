97336fcac36e1993653db14c2b66a1a162cd2c62
tanvii-18 <8438tanvipatel@gmail.com>
2026-01-07 10:43:21 +0530

    7-1-26

--- nodeMailer/OTP-nodemailer/controller/otpController.js
+++ nodeMailer/OTP-nodemailer/controller/otpController.js
@@ -18,4 +18,22 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-export const sendOTP = () => {};
+export const sendOTP = async (req, res) => {
+  const { email } = req.body;
+  const otp = generateOtp();
+  const expiryAt = new Date(Date.now() + 1000 * 60 * 2);
+
+  try {
+    await transporter.sendMail({
+      from: `OTP Verification !! <${process.env.EMAIL}>`,
+      to: email,
+      subject: "OTP Testing !",
+      text: `Your otp for verification is ${otp}, valid upto 2 minutes`,
+    });
+    await OTP.create({ email, otp, expiryAt });
+
+    res.json({ message: "OTP Sent successfully !!" });
+  } catch (err) {
+    res.json({ message: "otp not sent !", err });
+  }
+};
